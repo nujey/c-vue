77231b4f16381d4a0cfaf71bff3560c414d29fb5
张锋 <zhangfeng@xkeshi.com>
2018-04-15 08:48:06 +0800

    SSR

--- src/index.js
+++ src/index.js
@@ -8,12 +8,34 @@ import router from './router'
 import './components'
 import './plugins'
 import './assets/css/index.styl'
-// import JsEncrypt from 'jsencrypt/bin/jsencrypt'
 
 Vue.config.productionTip = false
 
-// Vue.prototype.$JsEncrypt = JsEncrypt
-
+// const server = require('express')()
+// const render = require('vue-server-renderer').createRenderer()
+// import server from 'express'
+// server.get('*', (req, res) => {
+//   const app = new Vue({
+//     data: {
+//       url: req.url
+//     },
+//     template: `<div>访问的 URL 是： {{ url }}</div>`
+//   })
+//   renderer.renderToString(app, (err, html) => {
+//     if (err) {
+//       res.status(500).end('Internal Server Error')
+//       return
+//     }
+//     res.end(`
+//       <!DOCTYPE html>
+//       <html lang="en">
+//         <head><title>Hello</title></head>
+//         <body>${html}</body>
+//       </html>
+//     `)
+//   })
+// })
+// server.listen(8080)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
